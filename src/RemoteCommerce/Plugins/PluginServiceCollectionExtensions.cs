5a2534f809395d502580caf88ac0e2717e90c5bf
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-08-12 08:56:18 -0300

    stage 02: keep plugin bootstrap dependency injection based

--- src/RemoteCommerce/Plugins/PluginServiceCollectionExtensions.cs
+++ src/RemoteCommerce/Plugins/PluginServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Logging;
+using RemoteCommerce.Infrastructure.Persistence;
 
 namespace RemoteCommerce.Plugins;
 
@@ -8,17 +9,22 @@ namespace RemoteCommerce.Plugins;
 public static class PluginServiceCollectionExtensions
 {
     /// <summary>
-    /// Registers the plugin installation service and loads installed plugin extensions into the service collection.
+    /// Registers the plugin installation service and creates the startup plugin loader from the configured host dependencies.
     /// </summary>
     /// <param name="services">The application service collection.</param>
     /// <param name="pluginsRoot">The root directory containing installed plugin packages.</param>
     /// <param name="configuration">The application configuration used to configure plugin logging.</param>
+    /// <remarks>
+    /// The loader executes before <see cref="WebApplicationBuilder.Build"/> so plugin registrations become part of the final service provider.
+    /// </remarks>
     public static void AddInstalledRemoteCommercePlugins(this IServiceCollection services, string pluginsRoot, IConfiguration configuration)
     {
         services.AddScoped<PluginInstallationService>();
 
         using var loggerFactory = LoggerFactory.Create(logging => logging.AddConfiguration(configuration.GetSection("Logging")));
-        var loader = new PluginLoader(loggerFactory.CreateLogger<PluginLoader>());
+        var logger = loggerFactory.CreateLogger<PluginLoader>();
+        var dbFactory = services.BuildServiceProvider().GetRequiredService<IDbContextFactory<CommerceDbContext>>();
+        var loader = new PluginLoader(logger, dbFactory);
         loader.Load(services, pluginsRoot);
     }
 }
