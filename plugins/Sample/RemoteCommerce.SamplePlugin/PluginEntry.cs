2ba159f7cf066adb255c0714102ce98f463dcd98
Weslley Murdock <weslleyluiz@outlook.com.br>
2026-08-12 11:24:00 -0300

    feat: improve plugin

--- plugins/Sample/RemoteCommerce.SamplePlugin/PluginEntry.cs
+++ plugins/Sample/RemoteCommerce.SamplePlugin/PluginEntry.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using RemoteCommerce.Plugins.Abstractions;
 
--- src/RemoteCommerce.Plugin.Abstractions/PluginManifest.cs
+++ src/RemoteCommerce.Plugin.Abstractions/PluginManifest.cs
@@ -5,6 +5,8 @@ namespace RemoteCommerce.Plugins.Abstractions;
 /// </summary>
 /// <param name="Id">The stable unique identifier of the plugin.</param>
 /// <param name="Name">The human-readable plugin name.</param>
+/// <param name="License">The license file path or string expression name of the plugin.</param>
+/// <param name="Readme">The readme file path of the plugin.</param>
 /// <param name="Version">The semantic version of the plugin package.</param>
 /// <param name="EntryAssembly">The package-relative path to the plugin entry assembly.</param>
 /// <param name="EntryType">The fully qualified type implementing <see cref="IRemoteCommercePlugin"/>.</param>
@@ -13,6 +15,8 @@ namespace RemoteCommerce.Plugins.Abstractions;
 public sealed record PluginManifest(
     string Id,
     string Name,
+    string License,
+    string Readme,
     string Version,
     string EntryAssembly,
     string EntryType,
