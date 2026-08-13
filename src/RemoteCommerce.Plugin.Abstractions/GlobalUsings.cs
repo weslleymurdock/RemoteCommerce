2faa7d05e5552a5d2a52b56c77148c0c4e11a44c
Weslley Murdock <weslleyluiz@outlook.com.br>
2026-08-13 20:29:39 -0300

    fix: add GlobalUsings.cs in Plugins.Abstractions project

---
+++ src/RemoteCommerce.Plugin.Abstractions/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using System.Collections.Generic;
+global using System.Text.Json.Serialization;
