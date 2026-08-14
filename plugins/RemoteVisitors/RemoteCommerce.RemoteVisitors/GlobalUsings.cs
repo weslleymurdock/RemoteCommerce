f23bbe404c2b56bb2d24c0111c805574571ef75c
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-08-14 03:09:22 -0300

    fix(plugins): complete RemoteVisitors global imports

--- plugins/RemoteVisitors/RemoteCommerce.RemoteVisitors/GlobalUsings.cs
+++ plugins/RemoteVisitors/RemoteCommerce.RemoteVisitors/GlobalUsings.cs
@@ -1,8 +1,11 @@
 global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Migrations;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
 global using MudBlazor;
 global using RemoteCommerce.Plugins.Abstractions;
 global using System.Security.Cryptography;
