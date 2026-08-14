80eaee34f2a034cd45c1e6bef81294c190286824
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-08-14 00:14:34 -0300

    fix(build): centralize plugin loader reflection namespaces

--- src/RemoteCommerce/GlobalUsings.cs
+++ src/RemoteCommerce/GlobalUsings.cs
@@ -75,6 +75,8 @@ global using System.Globalization;
 global using System.IdentityModel.Tokens.Jwt;
 global using System.Linq;
 global using System.Net;
+global using System.Reflection;
+global using System.Runtime.Loader;
 global using System.Security.Claims;
 global using System.Security.Cryptography;
 global using System.Text;
